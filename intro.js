f19225b7988755a6cf2cced67551410e2cb71e6f
Souhardya Chakraborty <64438285+souhardya1@users.noreply.github.com>
2021-10-29 06:44:09 +0530

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,15 @@
+console.time('Total time taken');
+console.log('Hello Console');
+console.log(4*3);
+console.log(true);
+console.log([10,20,30,40]);
+console.log({souhardya:"22",purbasha:20});
+console.table({souhardya:"22",purbasha:20});
+console.warn("1 warning");
+// console.clear()
+console.timeEnd('Total time taken');
+console.assert(17>18,'You can not vote')
+console.error("This is an error")
+
+/* This is a 
+multiline comment*/
